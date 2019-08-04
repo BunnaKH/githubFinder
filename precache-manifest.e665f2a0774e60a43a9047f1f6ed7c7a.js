839aeb45a8f82d61aff935a7114666965faa4c30
BunnaKH <bunnaq7@gmail.com>
2019-08-04 23:23:54 +0200

    Updates

---
+++ precache-manifest.e665f2a0774e60a43a9047f1f6ed7c7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7016135ef170525b53d86d45360b5da",
+    "url": "/githubfinder/index.html"
+  },
+  {
+    "revision": "69acfa30714c79293dd3",
+    "url": "/githubfinder/static/css/main.4d1c2e0d.chunk.css"
+  },
+  {
+    "revision": "a70cc7d5c876ecb555d0",
+    "url": "/githubfinder/static/js/2.a3e9d0b6.chunk.js"
+  },
+  {
+    "revision": "69acfa30714c79293dd3",
+    "url": "/githubfinder/static/js/main.2dcd7234.chunk.js"
+  },
+  {
+    "revision": "69af9ac4ea908b096fc3",
+    "url": "/githubfinder/static/js/runtime~main.9a344206.js"
+  }
+]);
